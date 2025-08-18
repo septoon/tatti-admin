5220acccc66c551be2caf8454ae758099b0ea0c0
Tigran <41443306+septoon@users.noreply.github.com>
2025-08-18 16:19:59 +0300

    added loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-import { useTelegramInit } from './lib/tg'
 import { NavTabs } from './components/NavTabs'
 import MenuPage from './features/menu/MenuPage'
 import ReviewsPage from './features/reviews/ReviewsPage'
@@ -8,35 +7,46 @@ import EasterPage from './features/easter/EasterPage'
 import ServicePackagesPage from './features/servicePackages/ServicePackagesPage'
 import InfoPage from './features/info/InfoPage'
 import WebApp from '@twa-dev/sdk'
+import Loader from './components/Loader/Loader'
 
 export default function App() {
+  const [loading, setLoading] = React.useState(true);
+
   useEffect(() => {
+    const init = async () => {
       try {
-        if (!WebApp) {
-          console.error('Telegram WebApp API недоступен.');
-          return;
-        }
         WebApp.ready();
         WebApp.expand();
-      } catch (error) {
-        console.error('Ошибка при инициализации Telegram WebApp:', error);
+        
+        await fetch('/ping'); 
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
       }
-    }, []);
+    };
+    init();
+  }, []);
+
   const [tab, setTab] = React.useState('menu')
 
   return (
     <div className="max-w-7xl mx-auto p-4 space-y-4">
-
-      <NavTabs value={tab} onChange={setTab} />
-
-      <div className='pt-20'>
-        {tab === 'menu' && <MenuPage />}
-        {tab === 'reviews' && <ReviewsPage />}
-        {tab === 'cakes' && <CakesPage />}
-        {tab === 'easter' && <EasterPage />}
-        {tab === 'servicePackages' && <ServicePackagesPage />}
-        {tab === 'info' && <InfoPage />}
-      </div>
+    {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <NavTabs value={tab} onChange={setTab} />
+          <div className='pt-20'>
+            {tab === 'menu' && <MenuPage />}
+            {tab === 'reviews' && <ReviewsPage />}
+            {tab === 'cakes' && <CakesPage />}
+            {tab === 'easter' && <EasterPage />}
+            {tab === 'servicePackages' && <ServicePackagesPage />}
+            {tab === 'info' && <InfoPage />}
+          </div>
+        </>
+      )}
     </div>
   )
 }
