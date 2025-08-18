f44d9bb2c301cc68b959ddd5d0b03b51d5f09c81
Tigran <41443306+septoon@users.noreply.github.com>
2025-08-18 15:26:09 +0300

    network/TLS check

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import axios from 'axios'
+import { WebApp } from '@twa-dev/sdk'
 import { useTelegramInit } from './lib/tg'
 import { NavTabs } from './components/NavTabs'
 import MenuPage from './features/menu/MenuPage'
@@ -10,6 +12,26 @@ import InfoPage from './features/info/InfoPage'
 
 export default function App() {
   useTelegramInit()
+
+  React.useEffect(() => {
+    async function checkApi() {
+      try {
+        await axios.get('https://api.tatti-shef.ru/ping')
+      } catch (e: any) {
+        let msg = ''
+        if (e.response) {
+          msg = `HTTP ${e.response.status}`
+        } else if (e.request) {
+          msg = 'No response (network/TLS)'
+        } else {
+          msg = e.message
+        }
+        WebApp.showAlert(`API error: ${msg}`)
+      }
+    }
+    checkApi()
+  }, [])
+
   const [tab, setTab] = React.useState('menu')
 
   return (
